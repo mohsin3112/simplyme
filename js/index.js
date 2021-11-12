b59275f86b839fedb8d5cbcd4f27d1edf6e2333e
Mohsin Panchadia <panchadiamohsin1@gmail.com>
2021-11-12 18:11:29 +0530

    Simply Me- profile website

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+const toggleBtn = document.querySelector(".toggle-nav");
+const nav = document.querySelector(".nav-footer");
+toggleBtn.addEventListener("click", (e)=>{
+    nav.classList.toggle("show");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
